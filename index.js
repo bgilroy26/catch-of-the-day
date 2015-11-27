4386ca35b88d5ff7c71622b2628667937b09b496
bgilroy26 <bdgilroy@gmail.com>
2015-11-27 12:58:22 -0500

    first commit; import React

---
+++ index.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+/*
+ * StorePicker
+ * This will let ups make <StorePicker/>
+ */
+
+var StorePicker = React.createClass({
+    render: function(){
+        return (
+            <p>hi</p>
+            )
+    }
+});
