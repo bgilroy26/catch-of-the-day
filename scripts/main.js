6bd0f45727a2a38b5f21b6c08ab82a00cc5f6f20
bgilroy26 <bdgilroy@gmail.com>
2015-11-27 15:14:04 -0500

    added StorePicker+ReactDOM+index.html

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+/*
+ * StorePicker
+ * This will let ups make <StorePicker/>
+ */
+
+var StorePicker = React.createClass({
+    render: function(){
+        return (
+            <p>hi</p>
+            )
+    }
+});
+
+ReactDOM.render(<StorePicker/>)
